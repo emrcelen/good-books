1ca5a764f23dbbb900f3bfdc73fe6db97c9fe9cd
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2024-01-13 21:51:07 +0300

    🔄 Updated model classes for Book, Category and Author

--- persistence/src/main/java/com/realworld/model/Author.kt
+++ persistence/src/main/java/com/realworld/model/Author.kt
@@ -1,19 +1,34 @@
 package com.realworld.model
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToMany
 import jakarta.persistence.Table
+import org.hibernate.annotations.GenericGenerator
+import java.util.*
 
 @Entity(name = "Author")
 @Table(name = "author", schema = "good_books")
 data class Author(
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "author_id")
+    val id: UUID?,
     var authorName: String?,
-    var authorSurname: String?
+    var authorSurname: String?,
+    @ManyToMany(mappedBy = "authors")
+    var books: Set<Book>?,
 ) {
     protected constructor() : this(Builder())
 
     private constructor(builder: Builder) : this(
+        null,
         builder.authorName,
-        builder.authorSurname
+        builder.authorSurname,
+        builder.books
     )
 
     class Builder {
@@ -21,9 +36,12 @@ data class Author(
             private set
         var authorSurname: String? = ""
             private set
-
+        var books: Set<Book>? = null
+            private set
 
         fun authorName(authorName: String) = apply { this.authorName = authorName }
         fun authorSurname(authorSurname: String) = apply { this.authorSurname = authorSurname }
+        fun books(book:Set<Book>) = apply { this.books = books }
+        fun build() = Author(this)
     }
 }
--- persistence/src/main/java/com/realworld/model/BookCategories.kt
+++ persistence/src/main/java/com/realworld/model/BookCategories.kt
@@ -1,17 +1,25 @@
 package com.realworld.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+import jakarta.persistence.*
+import org.hibernate.annotations.GenericGenerator
+import java.util.*
 
 @Entity(name = "BookCategories")
 @Table(name = "bookcategory", schema = "good_books")
 data class BookCategories(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+    val id: Long?,
+    @Column(unique = true)
     val categoryName: String?,
+    @ManyToMany(mappedBy = "categories")
     var bookCategory: Set<Book>?
 ) {
     protected constructor() : this(Builder())
 
     private constructor(builder: Builder) : this(
+        null,
         builder.categoryName,
         builder.bookCategory
     )
@@ -24,7 +32,6 @@ data class BookCategories(
 
         fun categoryName(categoryName: String) = apply { this.categoryName = categoryName }
         fun bookCategory(bookCategory: Set<Book>) = apply { this.bookCategory = bookCategory }
+        fun build() = BookCategories(this)
     }
-
-
 }
