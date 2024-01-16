7a5855dbbc5663df89f850ff93c1695c876fbf8a
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2024-01-16 15:37:42 +0300

    Fix model classes

--- persistence/src/main/java/com/realworld/enums/Role.java
+++ persistence/src/main/java/com/realworld/enums/Role.java
@@ -3,8 +3,6 @@ package com.realworld.enums;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-@Entity (name = "Role")
-@Table (name = "role")
 public enum Role {
     ADMIN, RECOMMENDATORY, USER;
 }
--- persistence/src/main/java/com/realworld/model/Author.kt
+++ persistence/src/main/java/com/realworld/model/Author.kt
@@ -1,29 +1,35 @@
 package com.realworld.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity(name = "Author")
 @Table(name = "author", schema = "good_books")
 data class Author(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
     var authorName: String?,
     var authorSurname: String?
 ) {
     protected constructor() : this(Builder())
 
     private constructor(builder: Builder) : this(
+        null,
         builder.authorName,
         builder.authorSurname
     )
 
     class Builder {
+        var id: Long? = null
+            private set
         var authorName: String? = ""
             private set
         var authorSurname: String? = ""
             private set
 
-
+        fun id(id: Long) = apply { this.id = id }
         fun authorName(authorName: String) = apply { this.authorName = authorName }
         fun authorSurname(authorSurname: String) = apply { this.authorSurname = authorSurname }
+        fun build() = Author(this)
     }
 }
