175d85a57f77c4e20af87ce8ba753389ffb7ba20
Suleyman <suleymankisir@gmail.com>
2024-01-18 13:59:57 +0300

    Updated account class

--- persistence/src/main/java/com/realworld/model/Account.kt
+++ persistence/src/main/java/com/realworld/model/Account.kt
@@ -2,13 +2,13 @@ package com.realworld.model
 
 import com.realworld.enums.Role
 import jakarta.persistence.*
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User
 
 @Entity(name = "Account")
 @Table(name = "account", schema = "good_books")
 data class Account(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
     val id: Long?,
     @Column(unique = true)
     val username: String?,
@@ -17,10 +17,12 @@ data class Account(
     var password: String?,
     @Enumerated
     var role: Role?,
-    @OneToOne(mappedBy = "userfollow")
-    var following: Set<UserFollow>?,
-    @OneToOne
-    var followers: Set<UserFollow>?,
+    @ManyToMany(mappedBy = "followers")
+    var following: Set<Account>?,
+    @ManyToMany
+    @JoinTable(name = "account_followers", schema = "good_books", joinColumns = [JoinColumn (name = "account_id")],
+               inverseJoinColumns = [JoinColumn (name = "follower_id")])
+    var followers: Set<Account>?,
     @ManyToMany
     var favorites: Set<Book>?,
     var accountExpired: Boolean?,
@@ -54,9 +56,9 @@ data class Account(
             private set
         var role: Role? = null
             private set
-        var following: Set<UserFollow>? = null
+        var following: Set<Account>? = null
             private set
-        var followers: Set<UserFollow>? = null
+        var followers: Set<Account>? = null
             private set
         var favorites: Set<Book>? = null
             private set
@@ -72,8 +74,8 @@ data class Account(
         fun username(username: String) = apply { this.username = username }
         fun email(email: String) = apply { this.email = email }
         fun role(role: Role) = apply { this.role = role }
-        fun following(following: Set<UserFollow>) = apply { this.following = following }
-        fun followers(followers: Set<UserFollow>) = apply { this.followers = followers }
+        fun following(following: Set<Account>) = apply { this.following = following }
+        fun followers(followers: Set<Account>) = apply { this.followers = followers }
         fun favorites(favorites: Set<Book>) = apply { this.favorites = favorites }
         fun accountExpired(accountExpired: Boolean) = apply { this.accountExpired = accountExpired }
         fun accountLock(accountLock: Boolean) = apply { this.accountLock = accountLock }
